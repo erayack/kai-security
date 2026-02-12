d0702a7fe5b38d2a33249b4181dbd49a8a1fd8e7
andthattoo <omer@firstbatch.xyz>
2026-02-12 11:49:43 +0300

    cleanup

--- src/kai/agents/agent_types/__init__.py
+++
@@ -1,9 +0,0 @@
-from kai.agents.agent_types.setup_agent import SetupAgent
-from kai.agents.agent_types.profiler_agent import ProfilerAgent
-from kai.agents.agent_types.workspace_validation_agent import WorkspaceValidationAgent
-
-__all__ = [
-    "SetupAgent",
-    "ProfilerAgent",
-    "WorkspaceValidationAgent",
-]
